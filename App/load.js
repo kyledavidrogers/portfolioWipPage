5110b86ee917575a17ddd7a468df13c77e88b298
Kyle Rogers <krogers025@hotmail.com>
2024-03-03 20:33:59 -0500

    LoadingV1.1

--- App/load.js
+++ App/load.js
@@ -8,10 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
   function frame() {
     if (width >= 100) {
       clearInterval(loading);
-      var loadingWrapper = document.getElementById('loading-wrapper');
-      loadingWrapper.style.transition = 'transform 1s cubic-bezier(0.55, 0.055, 0.675, 0.19)'; // Using cubic-bezier for zippy effect
-      loadingWrapper.style.transform = 'translateY(100vh)';
-      setTimeout(() => loadingWrapper.remove(), 1000);
+      setTimeout(() => { // Delay before starting slide-down
+        var loadingWrapper = document.getElementById('loading-wrapper');
+        loadingWrapper.style.transition = 'transform 1s cubic-bezier(0.55, 0.055, 0.675, 0.19)';
+        loadingWrapper.style.transform = 'translateY(100vh)';
+        setTimeout(() => loadingWrapper.remove(), 1000);
+      }, 200); // 0.2s delay
     } else {
       width += 1.6;
       var progress = easingFunction(width / 100) * 100;
